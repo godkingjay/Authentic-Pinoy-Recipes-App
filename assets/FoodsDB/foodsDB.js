12020a66bcf8006f514ea7a83a62cca0835454ea
Jarrian <glorianagojar@gmail.com>
2022-10-25 21:27:15 +0800

    ADD: Food Database/Pork Sinigang

--- assets/FoodsDB/foodsDB.js
+++ assets/FoodsDB/foodsDB.js
@@ -1,130 +1,3 @@
-// const PinoyFoods = [
-//   {
-//     id: 1,
-//     name: 'Pork Sinigang',
-//     description: 'Sinigang is a sour soup native to the Philippines. This recipe uses pork as the main ingredient. Other proteins and seafood can also be used. Beef, shrimp, fish are commonly used to cook sinigang. The chicken version, on the other hand, is called sinampalukang manok. I prefer to use either pork belly or buto-buto when cooking sinigang. The latter refers to cuts with bones intact. These are either pork neck bones, chopped spare ribs, and chopped baby back ribs. Pork shoulder and ham can also be used when cooking sinigang.',
-//     link: 'https://panlasangpinoy.com/pork-sinigang-na-baboy-recipe/',
-//     informations: [
-//       {
-//         title: 'Souring Agents for Sinigang',
-//         content: `Of the many soups and stews that proliferate Filipino cuisine, it can be sure that sinigang has no rival. Many meals, both in childhood and adulthood, have been spent sitting in front of the familiarly flavorful broth. Its inviting aroma belies a flavor unlike no other — tart, savory, and refreshing, one can’t help but fall in love with it! With many heralding it as the national Filipino food, it comes as no surprise that its reputation has spread far and wide across the world.
-
-//         Indeed, in 2021, travel guide TasteAtlas proclaimed our humble dish to be the best vegetable soup in the world! This should come as no surprise to us, either. After all, who can resist the warmth and comfort that a bowl of sinigang brings? Although a staple so common in every household, knowing that sinigang will be the main course of the day can’t help but elicit feelings of excitement and delight. No matter the weather nor the season, sinigang stands both the test of time and climate. This dish brings pleasure wherever it finds a home, and Filipinos continue to relish in its sour, but yummy taste.`
-//       },
-//       {
-//         title: 'Where does sinigang come from?',
-//         content: `A mix of tender meat, fresh spinach, string beans, and tomatoes, as well as a dash (or more) of savory fish sauce, make for the perfect combination in simmering and incorporating each unique flavor with each other. Powerful and poignant, sinigang is a Filipino native, through and through. Its name, sinigang, originates from the Tagalog verb that directly translates to “to stew” — sigang! Because of its Tagalog name, many presume it to be Tagalog in origin as well. This yummy meal has relatives in other regions in the country as well, to be sure — yet none are quite like this classic soup that many have fallen in love with.
-
-//         Again, sinigang isn’t a dish so much as it is a cooking method. This is why, like we mentioned earlier, there are a wide number of variations of this delicious dish! These include your sinigang na hipon, a favorite seafood variant. Here, tender and fresh shrimp soak up all the vibrant flavors of your beloved stew. It’s important to keep your shrimp perfectly cooked, so it’s much easier to peel the skin off later on. 
-        
-//         And for something a little more out of the box? Try this sinigang mix inasal na manok! Now, these are two dishes you wouldn’t commonly associate with each other. But the thrilling flavors of a sinigang broth, coupled with the smoky taste of inasal, makes this a winning combination. This twist on another Filipino favorite breathes life into your everyday inasal recipe. Sample it for yourself!
-        
-//         Which sinigang variant is your favorite?`
-//       },
-//       {
-//         title: 'How to Cook Sinigang',
-//         content: `Pork Sinigang is delicious and easy to prepare. Note that this recipe will require to saute the onion and pork. Start by heating cooking oil in a cooking pot. Saute the onion until it gets soft. Add the sliced pork. We are using pork belly (or liempo) for this recipe. You may also use other pork slices such as shoulders or parts with bones. Add some fish sauce and continue to cook until the pork turns light brown.
-
-//         It is time to add the liquid. This recipe calls for water. You may want to use half water and half beef broth if you prefer your sinigang to be really flavorful. Let the liquid boil and then add the tomato and taro (gabi). The tomato needs to blend in with the broth while the taro will need to cook for a longer time so that it will start to release starch that will make the soup thicker.
-        
-//         The souring agent for this recipe is a tamarind soup base mix, which is more popularly known as  sinigang mix. This is a quicker alternative to fresh tamarind and it will save you time. Add the mix once meat is tender. Add some long green chili, if desired. This will make your soup spicy good. Add the rest of the vegetables towards the last few minutes. Make sure to add the spinach last.
-
-//         This is best served with a condiment composed of fish sauce and lots of hot chili pepper.`
-//       },
-//       {
-//         title: 'Sinigang Alternative Ingredients',
-//         content: `This sinigang version makes use of pork belly. Any cuts of pork can be used to make it. Beef and fish can also be utilized.
-
-//         I am using spinach in this recipe. It is actually an alternative ingredient to kangkong (water spinach), which is the most common type of leafy green vegetable used when cooking sinigang.
-        
-//         Sinigang mix is a powdered product that helps bring-in that good sour taste to sinigang. Old school methods requires boiling either fresh young tamarind or bilimbi for a long time until the flavors are extracted. It is your choice. It is a no-brainer, so I chose flavor and convenience.`
-//       },
-//       {
-//         title: 'Tips in Cooking Pork Sinigang',
-//         content: `Make sure to cook the pork until very tender. I really mean it. You don’t want to challenge yourself by chewing the meat with all your might while the nice hot broth slowly cools down. Otherwise, enjoy the broth first before working on the meat.
-        
-//         Onion and tomato brings-in flavor to the broth. Cook it during the initial steps. Some people like their tomato intact. If you are one of these people, I suggest to cook half of the tomato initially and add the remaining once the dish is almost done.
-
-//         You can still make sinigang even without all the vegetable in the list. All you need is pork, green leafy veggies, and the souring agent. Of course, water is essential for this recipe.
-
-//         Busy people can use a slow cooker to cook sinigang. Make sure to add the spinach or kangkong only when you are about to eat. It is best to have these fresh.`
-//       },
-//       {
-//         title: 'Is sinigang really the national dish of the Philippines?',
-//         content: `Though many make jokes and proclamations to the same effect, the truth of the matter is that sinigang is only the Philippine’s national dish unofficially. Regardless, some dishes may also rise to the challenge of national dish, and have separate camps of their own! Let’s take a look at some of them now.
-
-//         For starters, of course, you have sinigang’s topmost competition: adobo! Point blank, adobo is perhaps sinigang’s antithesis: where the latter is sour and tart and soupy, adobo is a salty, savory and saucy dish. The only thing they have in common is their use of juicy and tender pork belly as the main protein. Like sinigang, the term adobo refers to a general cooking method, and not necessarily one particular meal. To adobo something means to marinate it in a combination of soy sauce, vinegar, garlic, and peppercorn. These flavors intermingle so perfectly, you wouldn’t be able to tell one from the other. The result is, to be sure, a dish that is so umami you’d want to drizzle the sauce over your entire plate of rice!`
-//       }
-//     ],
-//     recipe: {
-//       details: {
-//         description: 'Pork soup in sour broth with okra, kangkong, eggplant, and string beans. This dish is best during the cold weather.',
-//         course: 'Main Course',
-//         time: {
-//           prep: '10 Minutes',
-//           cook: '1 Hour',
-//           total: '1 Hour 10 minutes'
-//         },
-//         servings: '6 People',
-//         calories: '892kcal',
-//         author: 'Vanjo Merano'
-//       },
-//       ingredients: [
-//         { amount: '2 lbs', name: 'Pork Belly', type: null, image: 'pork' },
-//         { amount: '1 Bunch', name: 'Spinach', type: null, image: 'spinach' },
-//         { amount: '3 Tablespoons', name: 'Fish Sauce', type: null, image: 'fishSauce' },
-//         { amount: '12 Pieces', name: 'String Beans', type: 'Cut in 2 inch length', 'beans' },
-//         { amount: '2 Pieces', name: 'Tomato', type: 'quartered', image: 'tomato' },
-//         { amount: '3 Pieces', name: 'Chili/Banana Pepper', type: null, image: 'chili' },
-//         { amount: '1 Tablespoon', name: 'Cooking Oil', type: null, image: 'oil' },
-//         { amount: '2 Quarts', name: 'Water', type: null, image: 'water' },
-//         { amount: '1 Piece', name: 'Onion', type: 'sliced', image: 'onion' },
-//         { amount: '2 Pieces', name: 'Taro', type: 'quartered', image: 'taro' },
-//         { amount: '1 Pack per 2 liters of Water', name: 'sinigangMix', type: null, image: 'sinigangMix' },
-//       ],
-//       instructions: [
-//         'Heat the pot and put-in the cooking oil',
-//         'Sauté the onion until its layers separate from each other',
-//         'Add the pork belly and cook until outer part turns light brown',
-//         'Put-in the fish sauce and mix with the ingredients',
-//         'Pour the water and bring to a boil',
-//         'Add the taro and tomatoes then simmer for 40 minutes or until pork is tender',
-//         'Put-in the sinigang mix and chili',
-//         'Add the string beans (and other vegetables if there are any) and simmer for 5 to 8 minutes',
-//         'Put-in the spinach, turn off the heat, and cover the pot. Let the spinach cook using the remaining heat in the pot.',
-//         'Serve hot. Share and enjoy!',
-//       ],
-//       notes: [
-//         'This recipe can also be used for beef, chicken, and fish.',
-//         'Souring agents also include tamarind, bilimbi, unripe mango, and calamansi. I used “sinigang mix” for this recipe  for convenience. It also gives the exact amount of sourness that I like for my sinigang.',
-//         'Taro is nice to have. It will give your soup a thicker texture. However, you can still make this dish without it.',
-//         'Sliced daikon radish are used in some version. Feel free to add some if preferred.',
-//         'I used spinach for this recipe. A more traditional approach is the use of water spinach (kangkong).',
-//         'If spinach or water spinach are not available, you can use bok choy.',
-//       ],
-//       nutrition: [
-//         { name: 'Serving', amount: '6g' },
-//         { name: 'Calories', amount: '892kcal' },
-//         { name: 'Carbohydrates', amount: '18g' },
-//         { name: 'Protein', amount: '20g' },
-//         { name: 'Fat', amount: '83g' },
-//         { name: 'Saturated Fat', amount: '29g' },
-//         { name: 'Cholesterol', amount: '108mg' },
-//         { name: 'Sodium', amount: '1067mg' },
-//         { name: 'Potassium', amount: '1070mg' },
-//         { name: 'Fiber', amount: '6g' },
-//         { name: 'Sugar', amount: '8g' },
-//         { name: 'Vitamin A', amount: '6710IU' },
-//         { name: 'Vitamin C', amount: '42.8mg' },
-//         { name: 'Calcium', amount: '157mg' },
-//         { name: 'Iron', amount: '4.5mg' },
-//       ],
-//     },
-//     video: 'https://youtu.be/kIoY0li1ABk',
-//     image: '',
-//   }
-// ];
-
 const PinoyFoods = [
   {
     id: 1,
@@ -2368,6 +2241,71 @@ const PinoyFoods = [
     video: `https://youtu.be/hM24u0gRjIM`,
     image: require('./images/food/000000022.jpg'),
   },
+  {
+    id: 23,
+    name: `Pork Sinigang`,
+    tagalog: `Sinigang na Baboy`,
+    type: [
+      `Main Course`,
+      `Meat Dish`,
+      `Pork Dish`
+    ],
+    description: `Sinigang is a sour soup native to the Philippines. This recipe uses pork as the main ingredient. Other proteins and seafood can also be used. Beef, shrimp, fish are commonly used to cook sinigang. The chicken version, on the other hand, is called sinampalukang manok. I prefer to use either pork belly or buto-buto when cooking sinigang. The latter refers to cuts with bones intact. These are either pork neck bones, chopped spare ribs, and chopped baby back ribs. Pork shoulder and ham can also be used when cooking sinigang.`,
+    link: `https://panlasangpinoy.com/pork-sinigang-na-baboy-recipe/`,
+    author: `Vanjo Merano`,
+    information: [
+      {
+        title: null,
+        content: null,
+      },
+    ],
+    recipe: {
+      details: {
+        description: null,
+        time: {
+          prep: null,
+          cook: null,
+          total: null,
+        },
+        servings: null,
+        calories: null,
+      },
+      ingredients: [
+        {
+          name: null,
+          amount: null,
+          tagalog: null,
+          type: null,
+          image: null,
+        },
+      ],
+      instructions: [
+        
+      ],
+      nutrition: [
+        { name: 'Serving', amount: '5g' },
+        { name: 'Calories', amount: '650kcal' },
+        { name: 'Carbohydrates', amount: '10g' },
+        { name: 'Protein', amount: '37g' },
+        { name: 'Fat', amount: '51g' },
+        { name: 'Saturated Fat', amount: '16g' },
+        { name: 'Polyunsaturated Fat', amount: '6g' },
+        { name: 'Monounsaturated Fat', amount: '25g' },
+        { name: 'Trans Fat', amount: '1g' },
+        { name: 'Cholesterol', amount: '196mg' },
+        { name: 'Sodium', amount: '868mg' },
+        { name: 'Potassium', amount: '888mg' },
+        { name: 'Fiber', amount: '3g' },
+        { name: 'Sugar', amount: '5g' },
+        { name: 'Vitamin A', amount: '5187IU' },
+        { name: 'Vitamin C', amount: '57mg' },
+        { name: 'Calcium', amount: '59mg' },
+        { name: 'Iron', amount: '4mg' },
+      ],
+    },
+    video: `https://youtu.be/kIoY0li1ABk`,
+    image: require('./images/food/000000023.jpg'),
+  },
 ];
 
 
@@ -2464,7 +2402,8 @@ List:
 19.   Shanghai Lumpia
 20.   Caldereta
 21.   Kare-kare
-22.   Tortang Talong 
+22.   Tortang Talong
+23.   Pork Sinigang 
 
 **************************************************************************************************************/
 
