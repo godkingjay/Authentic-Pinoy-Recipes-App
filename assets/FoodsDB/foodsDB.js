d2ee09bd7c0bf47b5eabe7744c0c27731dd993b8
Jarrian <glorianagojar@gmail.com>
2022-10-28 17:08:00 +0800

    ADD: Food Database/Leche Flan/Recipe/Instructions

--- assets/FoodsDB/foodsDB.js
+++ assets/FoodsDB/foodsDB.js
@@ -2671,7 +2671,18 @@ const PinoyFoods = [
         },
       ],
       instructions: [
-        
+        `Using all the eggs, separate the yolk from the egg white (only egg yolks will be used).`,
+        `Place the egg yolks in a big bowl then beat them using a fork or an egg beater.`,
+        `Add the condensed milk and mix thoroughly.`,
+        `Pour-in the fresh milk and Vanilla. Mix well.`,
+        `Put the mold (llanera) on top of the stove and heat using low fire.`,
+        `Put-in the granulated sugar on the mold and mix thoroughly until the solid sugar turns into liquid (caramel) having a light brown color. Note: Sometimes it is hard to find a Llanera (Traditional flan mold) depending on your location. I find it more convenient to use individual Round Pans in making leche flan.`,
+        `Spread the caramel (liquid sugar) evenly on the flat side of the mold.`,
+        `Wait for 5 minutes then pour the egg yolk and milk mixture on the mold.`,
+        `Cover the top of the mold using an Aluminum foil.`,
+        `Steam the mold with egg and milk mixture for 30 to 35 minutes.`,
+        `After steaming, let the temperature cool down then refrigerate.`,
+        `Serve for dessert. Share and Enjoy!`,
       ],
       nutrition: [
         { name: 'Serving', amount: '5g' },
