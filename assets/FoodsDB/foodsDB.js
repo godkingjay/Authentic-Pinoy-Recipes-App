97bc326197c61bc0f66f56ef517410476475266a
Jarrian <glorianagojar@gmail.com>
2022-11-01 12:07:46 +0800

    ADD: Food Database/Lechon Kawali

--- assets/FoodsDB/foodsDB.js
+++ assets/FoodsDB/foodsDB.js
@@ -3047,6 +3047,71 @@ const PinoyFoods = [
     video: `https://youtu.be/_XfpiCUOFZo`,
     image: require('./images/food/000000028.jpg'),
   },
+  {
+    id: 29,
+    name: `Lechon Kawali`,
+    tagalog: `Lechon Kawali`,
+    type: [
+      `Meat Dish`,
+      `Pork Dish`,
+      `Deep-Fried Dish`
+    ],
+    description: `Lechon kawali with a cup of rice please! This is my regular order at least once a week when I was in high school. Our school canteen serves one of the best versions of this dish. I like it best when the pork just got out of the pot. It is super crispy and hot.`,
+    link: `https://panlasangpinoy.com/lechon-kawali/`,
+    author: `Vanjo Merano`,
+    information: [
+      {
+        title: null,
+        content: null,
+      },
+    ],
+    recipe: {
+      details: {
+        description: null,
+        time: {
+          prep: null,
+          cook: null,
+          total: null,
+        },
+        servings: null,
+        calories: null,
+      },
+      ingredients: [
+        {
+          name: null,
+          amount: null,
+          tagalog: null,
+          type: null,
+          image: null,
+        },
+      ],
+      instructions: [
+        
+      ],
+      nutrition: [
+        { name: 'Serving', amount: '5g' },
+        { name: 'Calories', amount: '650kcal' },
+        { name: 'Carbohydrates', amount: '10g' },
+        { name: 'Protein', amount: '37g' },
+        { name: 'Fat', amount: '51g' },
+        { name: 'Saturated Fat', amount: '16g' },
+        { name: 'Polyunsaturated Fat', amount: '6g' },
+        { name: 'Monounsaturated Fat', amount: '25g' },
+        { name: 'Trans Fat', amount: '1g' },
+        { name: 'Cholesterol', amount: '196mg' },
+        { name: 'Sodium', amount: '868mg' },
+        { name: 'Potassium', amount: '888mg' },
+        { name: 'Fiber', amount: '3g' },
+        { name: 'Sugar', amount: '5g' },
+        { name: 'Vitamin A', amount: '5187IU' },
+        { name: 'Vitamin C', amount: '57mg' },
+        { name: 'Calcium', amount: '59mg' },
+        { name: 'Iron', amount: '4mg' },
+      ],
+    },
+    video: `https://youtu.be/87V0NIhr4VA`,
+    image: require('./images/food/000000029.jpg'),
+  },
 ];
 
 
@@ -3150,6 +3215,7 @@ List:
 26.   Turon
 27.   Pork Tocino
 28.   Crispy Pata
+29.   Lechon Kawali
 
 **************************************************************************************************************/
 
