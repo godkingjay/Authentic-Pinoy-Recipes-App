d7c96080faa990c79e3d94517c0b507e01452d71
Jarrian <glorianagojar@gmail.com>
2022-11-22 16:56:55 +0800

    ADD: Food View/Ingredients [FIX]

--- assets/component/foodRecipe.js
+++ assets/component/foodRecipe.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 import { FlatList } from "react-native";
 import { Text } from "react-native";
 import { View } from "react-native";
+import tableStyles from "../styles/tableStyles";
 import FoodRecipeDetails from "./foodRecipeDetails";
 
 export default function FoodRecipe({ recipe }) {
@@ -19,7 +20,28 @@ export default function FoodRecipe({ recipe }) {
               <Text style={ styles.ingredientNumber }>{ ingredientNum }</Text>
             </View>
             <View style={ styles.ingredientDetails }>
-              <Text>{ ingredient.name }</Text>
+              <Text style={{ fontSize: 16, fontWeight: 'bold', color: '#222' }}>{ ingredient.name }</Text>
+              {ingredient.tagalog != null ? (
+                <View>
+                  <Text style={{ fontSize: 12, marginTop: -4, color: '#444', fontStyle: 'italic' }}>{ ingredient.tagalog }</Text>
+                </View>
+              ) : (
+                null
+              )}
+              <View style={ [tableStyles.container, tableStyles.border, { marginTop: 8, width: '100%' }] }>
+                {ingredient.type != null ? (
+                  <View style={ [styles.twoColumns, styles.rowDivider] }>
+                    <Text style={ styles.leftColumn }>Type</Text>
+                    <Text style={ styles.rightColumn }>{ ingredient.type }</Text>
+                  </View>
+                ) : (
+                  null
+                )}
+                <View style={ styles.twoColumns }>
+                  <Text style={ styles.leftColumn }>Amount</Text>
+                  <Text style={ styles.rightColumn }>{ ingredient.amount }</Text>
+                </View>
+              </View>
             </View>
           </View>
         );
@@ -59,9 +81,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   ingredientNumberContainer: {
-    height: 64,
+    height: 96,
+    width: 64,
     backgroundColor: '#36C464',
-    aspectRatio: 1,
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 4,
@@ -73,5 +95,30 @@ const styles = StyleSheet.create({
   },
   ingredientDetails: {
     padding: 8,
+    flex: 1
+  },
+  twoColumns: {
+    flex: 1,
+    flexDirection: 'row',
+    borderColor: "#444"
+  },
+  leftColumn: {
+    flex: 1,
+    textAlign: 'right',
+    paddingHorizontal: 8,
+    fontWeight: 'bold',
+    borderRightWidth: 1,
+    fontSize: 12
+  },
+  rightColumn: {
+    flex: 3.25,
+    textAlign: 'center',
+    fontStyle: 'italic',
+    flexWrap: 'wrap',
+    fontSize: 12
+  },
+  rowDivider: {
+    borderBottomWidth: 1,
+    borderColor: '#222',
   }
 });
