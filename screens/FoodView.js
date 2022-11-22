7f02e800e516f95bdbf42ac60a45209bf1d2a8d6
Jarrian <glorianagojar@gmail.com>
2022-11-22 11:22:01 +0800

    ADD: Discover/Horizontal Card -> Food View/Header Title

--- screens/FoodView.js
+++ screens/FoodView.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
 import { Text } from "react-native";
 import { View } from "react-native";
 
-export default function FoodView({ navigation, route, food }){
+export default function FoodView({ navigation, route }){
+  const [food, setFood] = useState(route.params);
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: food.name
+    });
+  }, [navigation])
+  
   return(
     <View>
-      <Text>Food View</Text>
+      <Text>{ food.name }</Text>
     </View>
   );
 }
