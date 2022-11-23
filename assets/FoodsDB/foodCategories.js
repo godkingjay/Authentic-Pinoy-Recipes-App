fd6ce799daa0355a77f76c8efdd50c261d5c6236
Jarrian <glorianagojar@gmail.com>
2022-11-23 19:17:25 +0800

    ADD: Food Database | Food Categories

--- assets/FoodsDB/foodCategories.js
+++ assets/FoodsDB/foodCategories.js
@@ -1,84 +1,111 @@
 const foodCategory = [
   {
     name: 'Appetizer',
+    image: null,
   },
   {
     name: 'Barbeque',
+    image: null,
   },
   {
     name: 'Beef Dish',
+    image: null,
   },
   {
     name: 'Breakfast',
+    image: null,
   },
   {
     name: 'Cake',
+    image: null,
   },
   {
     name: 'Chicken Dish',
+    image: null,
   },
   {
     name: 'Custard',
+    image: null,
   },
   {
     name: 'Deep-Fried Dessert',
+    image: null,
   },
   {
     name: 'Deep-Fried Dish',
+    image: null,
   },
   {
     name: 'Dessert',
+    image: null,
   },
   {
     name: 'Egg Dish',
+    image: null,
   },
   {
     name: 'Fish Dish',
+    image: null,
   },
   {
     name: 'Main Course',
+    image: null,
   },
   {
     name: 'Meat Dish',
+    image: null,
   },
   {
     name: 'Noodle Dish',
+    image: null,
   },
   {
     name: 'Offal Dish',
+    image: null,
   },
   {
     name: 'Pork Dish',
+    image: null,
   },
   {
     name: 'Porridge',
+    image: null,
   },
   {
     name: 'Seafood Dish',
+    image: null,
   },
   {
     name: 'Shrimp Dish',
+    image: null,
   },
   {
     name: 'Side Dish',
+    image: null,
   },
   {
     name: 'Snack',
+    image: null,
   },
   {
     name: 'Soup',
+    image: null,
   },
   {
     name: 'Stew',
+    image: null,
   },
   {
     name: 'Street Food',
+    image: null,
   },
   {
     name: 'Sweets',
+    image: null,
   },
   {
     name: 'Vegetable Dish',
+    image: null,
   },
 ];
 
