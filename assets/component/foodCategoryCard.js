1bb3dc07480101302542a44c59b5c1eed02a8a3a
Jarrian <glorianagojar@gmail.com>
2022-11-24 17:27:00 +0800

    ADD: Categories | Card

--- assets/component/foodCategoryCard.js
+++ assets/component/foodCategoryCard.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function FoodCategoryCard({ category }){
   return(
-    <Text>{ category.name }</Text>
+    <View style={ styles.cardWrapper }>
+      <View style={ styles.cardContainer }>
+        <View style={ styles.cardLabelContainer }>
+          <Text>{ category.name }</Text>
+        </View>
+        <View style={ styles.cardImageContainer }>
+          <Text>Image</Text>
+        </View>
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  cardWrapper: {
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    overflow: 'hidden',
+    borderRadius: 8,
+    elevation: 4,
+  },
+  cardLabelContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  cardImageContainer: {
+    height: 96,
+    backgroundColor: 'blue',
+    aspectRatio: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
