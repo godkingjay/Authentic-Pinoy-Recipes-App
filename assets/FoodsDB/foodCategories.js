675121f6634298a1ebb761bb848ef9ca395b0ad2
Jarrian <glorianagojar@gmail.com>
2022-11-24 19:25:52 +0800

    ADD: Categories | Card/Style/Colors

--- assets/FoodsDB/foodCategories.js
+++ assets/FoodsDB/foodCategories.js
@@ -2,30 +2,37 @@ const foodCategory = [
   {
     name: 'Appetizer',
     image: null,
+    color: '#ADE792',
   },
   {
     name: 'Barbeque',
     image: null,
+    color: '#A6523D',
   },
   {
     name: 'Beef Dish',
     image: null,
+    color: '#FF6464',
   },
   {
     name: 'Breakfast',
     image: null,
+    color: '#FFCE45',
   },
   {
     name: 'Cake',
     image: null,
+    color: '#DBD0C0',
   },
   {
     name: 'Chicken Dish',
     image: null,
+    color: '#FD841F',
   },
   {
     name: 'Custard',
     image: null,
+    color: '#FFCE45',
   },
   {
     name: 'Deep-Fried Dessert',
@@ -38,14 +45,17 @@ const foodCategory = [
   {
     name: 'Dessert',
     image: null,
+    color: '#009EFF',
   },
   {
     name: 'Egg Dish',
     image: null,
+    color: '#FFCE45',
   },
   {
     name: 'Fish Dish',
     image: null,
+    color: '#3E7DFF',
   },
   {
     name: 'Main Course',
@@ -54,59 +64,72 @@ const foodCategory = [
   {
     name: 'Meat Dish',
     image: null,
+    color: '#FF6464',
   },
   {
     name: 'Noodle Dish',
     image: null,
+    color: '#E6CBA8',
   },
   {
     name: 'Offal Dish',
     image: null,
+    color: '#B73E3E',
   },
   {
     name: 'Pork Dish',
     image: null,
+    color: '#FF6464',
   },
   {
     name: 'Porridge',
     image: null,
+    color: '#D0B8A8',
   },
   {
     name: 'Seafood Dish',
     image: null,
+    color: '#3E7DFF',
   },
   {
     name: 'Shrimp Dish',
     image: null,
+    color: '#CE4017',
   },
   {
     name: 'Side Dish',
     image: null,
+    color: '#FD841F',
   },
   {
     name: 'Snack',
     image: null,
+    color: '#FD841F',
   },
   {
     name: 'Soup',
     image: null,
+    color: '#87A2FB',
   },
   {
     name: 'Stew',
     image: null,
+    color: '#A1C298',
   },
   {
     name: 'Street Food',
     image: null,
+    color: '#B73E3E',
   },
   {
     name: 'Sweets',
     image: null,
+    color: '#FF8787',
   },
   {
     name: 'Vegetable Dish',
     image: null,
-    color: '#34C363'
+    color: '#34C363',
   },
 ];
 
