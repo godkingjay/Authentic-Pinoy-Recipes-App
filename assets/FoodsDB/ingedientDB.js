0ea48abce46b1a3edb00a28118d421159f04354b
Jarrian <glorianagojar@gmail.com>
2022-11-30 17:29:27 +0800

    ADD: Food View | Food Ingredient Card | Images [15]

--- assets/FoodsDB/ingedientDB.js
+++ assets/FoodsDB/ingedientDB.js
@@ -4,12 +4,24 @@ const Ingredients = [
     image: require('./images/ingredient/black-pepper.png'),
   },
   {
+    name: 'carrot',
+    image: require('./images/ingredient/carrot.png'),
+  },
+  {
     name: 'chicken',
     image: require('./images/ingredient/chicken.png'),
   },
   {
     name: 'egg',
     image: require('./images/ingredient/egg.png'),
+  },
+  {
+    name: 'fish-sauce',
+    image: require('./images/ingredient/fish-sauce.png'),
+  },
+  {
+    name: 'garlic',
+    image: require('./images/ingredient/garlic.png'),
   },
   {
     name: 'ginger',
@@ -22,6 +34,14 @@ const Ingredients = [
   {
     name: 'onion',
     image: require('./images/ingredient/onion.png'),
+  },
+  {
+    name: 'papaya',
+    image: require('./images/ingredient/papaya.png'),
+  },
+  {
+    name: 'red-bell-pepper',
+    image: require('./images/ingredient/red-bell-pepper.png'),
   },
   {
     name: 'rice',
