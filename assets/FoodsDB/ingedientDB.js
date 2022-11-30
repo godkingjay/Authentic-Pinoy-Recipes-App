1626a444fd32069ca82eee3c2889e18a991edbe6
Jarrian <glorianagojar@gmail.com>
2022-11-30 17:16:20 +0800

    ADD: Food View | Food Ingredient Card | Images [5]

--- assets/FoodsDB/ingedientDB.js
+++ assets/FoodsDB/ingedientDB.js
@@ -1,7 +1,23 @@
 const Ingredients = [
   {
+    name: 'chicken',
+    image: require('./images/ingredient/chicken.png'),
+  },
+  {
     name: 'egg',
     image: require('./images/ingredient/egg.png'),
+  },
+  {
+    name: 'milk-can',
+    image: require('./images/ingredient/milk-can.png'),
+  },
+  {
+    name: 'sugar',
+    image: require('./images/ingredient/sugar.png'),
+  },
+  {
+    name: 'vanilla',
+    image: require('./images/ingredient/vanilla.png'),
   }
 ];
 
